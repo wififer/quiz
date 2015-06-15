4b2abf9a8e0423df7811e0f9d47ac2f968f470a0
Fernando Ransanz <subdireccion.archivos.spain@gmail.com>
2015-06-15 12:53:42 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Creditos' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
 
 // Definición de rutas de quizes
 router.get('/quizes', quizController.index);
