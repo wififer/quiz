41fcf2019de02eeedd915f9f4d025f5e4924ef31
Fernando Ransanz <subdireccion.archivos.spain@gmail.com>
2015-06-15 12:19:07 +0200

    multiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,13 @@ router.get('/', function(req, res) {
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Creditos' });
 });
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+
+
+// Definición de rutas de quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
 
 
 module.exports = router;
