6539dc20f0282cdc6ef590812328a54b67cc9b4d
Fernando Ransanz <subdireccion.archivos.spain@gmail.com>
2015-06-18 14:30:08 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.param('quizId', quizController.load); // autoload :quizId
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 
 
