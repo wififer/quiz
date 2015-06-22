96da00a9df48d46e8d7e605726b220b7495991ac
Fernando Ransanz <subdireccion.archivos.spain@gmail.com>
2015-06-22 12:27:27 +0200

    presentar formulario de edición con tema guardado en BD

--- models/models.js
+++ models/models.js
@@ -36,10 +36,10 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count) {
     if (count=== 0) { // la tabla se inicializa solo si está vacía
     Quiz.create({ pregunta: 'Capital de Italia',
-                  tema: 'Humanidades',
+                  tema: 'humanidades',
                   respuesta: 'Roma'});
     Quiz.create({ pregunta: 'Capital de Portugal',
-                  tema: 'Humanidades',
+                  tema: 'humanidades',
                   respuesta: 'Lisboa'})
     .then(function(){console.log('Base de datos inicializada')});
   };
