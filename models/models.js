354da5a5ac0f633dec246da8ca99a69fff84a6bf
Fernando Ransanz <subdireccion.archivos.spain@gmail.com>
2015-06-23 12:49:10 +0200

    Cambios en formulario y pantalla de preguntas

--- models/models.js
+++ models/models.js
@@ -36,10 +36,10 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count) {
     if (count=== 0) { // la tabla se inicializa solo si está vacía
     Quiz.create({ pregunta: 'Capital de Italia',
-                  tema: 'humanidades',
+                  tema: 'Humanidades',
                   respuesta: 'Roma'});
     Quiz.create({ pregunta: 'Capital de Portugal',
-                  tema: 'humanidades',
+                  tema: 'Humanidades',
                   respuesta: 'Lisboa'})
     .then(function(){console.log('Base de datos inicializada')});
   };
