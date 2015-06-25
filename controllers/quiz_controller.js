f82877f0d6471d4451d721a6364b794a43f27190
Fernando Ransanz <subdireccion.archivos.spain@gmail.com>
2015-06-25 09:48:55 +0200

    Comentarios a las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,10 @@ var models = require('../models/models.js');
 
 // AUTOLOAD
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId).then(
+  models.Quiz.find({
+    where: { id: Number(quizId) },
+    include: [{ model: models.Comment }]
+  }).then(
     function(quiz)
 {
   if (quiz) {
--- models/models.js
+++ models/models.js
@@ -28,8 +28,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
+// Importar definición de la tabl Cooment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
   //  then(...) ejecuta el manejador una vez creada la tabla
